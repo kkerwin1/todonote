490f02bfa807948bd43c2b6ca33e394487d6a89e
kkerwin1 <kkerwin1@gmail.com>
2015-09-12 22:06:58 -0400

    learning 2

--- evernote/learning.py
+++ evernote/learning.py
@@ -1,4 +1,5 @@
-from devToken import token as _token	
+from devToken import token as _token
+from evernote import EvernoteClient
 
 client = EvernoteClient(token=_token)
 userToken = client.get_user_store()
